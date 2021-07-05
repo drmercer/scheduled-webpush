94bb29b79b7c83d98b5835602669cf3409e49aa4
Dan Mercer <danielmercer1@gmail.com>
2021-07-05 17:41:51 -0600

    setTimeout and send Slack message

--- register.ts
+++ register.ts
@@ -1,9 +1,26 @@
 import { injectable } from "./deps/injector.ts";
+import { sendSlackMessage } from './slack-notify.ts';
 
 export const RegisterHandler = injectable(inject => {
   return (url: URL, body: unknown): { body: string, status: number } => {
+    const id = Math.random().toString(36).substr(2);
+    const hook = url.searchParams.get('hook');
+    const delay = Number(url.searchParams.get('delay'));
+
+    if (!hook || !delay) {
+      return { body: "Bad Request", status: 400 };
+    }
+
+    const scheduledAt = new Date().toLocaleString();
+    setTimeout(async () => {
+      const sentAt = new Date().toLocaleString();
+      const success = await sendSlackMessage(hook, `${id}: Message scheduled at ${scheduledAt} and sent at ${sentAt}.`);
+      console.log(`${id}: fired`, { success });
+    }, delay);
+    console.log(`${id}: scheduled`);
+
     return {
-      body: "yeet " + JSON.stringify(body, null, 2),
+      body: `${id}: Scheduled at ${scheduledAt}.`,
       status: 200,
     };
   };
