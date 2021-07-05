7ce393deb52e5d2f55e539e26060152e6dd20cc4
Dan Mercer <danielmercer1@gmail.com>
2021-07-05 14:10:34 -0600

    Remove Slack stuff
    It was just for testing setTimeout() on Deno Deploy, so it's no longer
    needed.

--- main.ts
+++ main.ts
@@ -1,31 +1,13 @@
 
 import { serve, ServerRequest, Response } from "https://deno.land/std@0.100.0/http/server.ts";
-import { sendSlackMessage } from './slack-notify.ts';
 
 async function handle(request: ServerRequest): Promise<Response> {
   const url = new URL(request.url, `http://${request.headers.get("host")}`);
   const id = Math.random().toString(36).substr(2);
 
-  if (url.pathname === '/slack') {
-    const hook = url.searchParams.get('hook');
-    const delay = Number(url.searchParams.get('delay'));
+  // TODO implement /register and /vapidPublicKey
 
-    if (!hook || !delay) {
-      return { body: "Bad Request", status: 400 };
-    }
-
-    const scheduledAt = new Date().toLocaleString();
-    setTimeout(async () => {
-      const sentAt = new Date().toLocaleString();
-      const success = await sendSlackMessage(hook, `${id}: Message scheduled at ${scheduledAt} and sent at ${sentAt}.`);
-      console.log(`${id}: fired`, {success});
-    }, delay);
-    console.log(`${id}: scheduled`);
-
-    return { body: "Scheduled. ID = " + id };
-  } else {
-    return { body: "Not found", status: 404 };
-  }
+  return { body: "Not found", status: 404 };
 }
 
 async function handleAndRespond(request: ServerRequest): Promise<void> {
