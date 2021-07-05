6fd792d3ac6c3fc0559ad1e0b57aec54bf99f6ec
Dan Mercer <danielmercer1@gmail.com>
2021-07-05 13:41:40 -0600

    Rewrite server to use std/http
    Moving from Deno Deploy to OpeNode

--- main.ts
+++ main.ts
@@ -1,7 +1,9 @@
+
+import { serve, ServerRequest, Response } from "https://deno.land/std@0.100.0/http/server.ts";
 import { sendSlackMessage } from './slack-notify.ts';
 
-async function handle(request: Request): Promise<Response> {
-  const url = new URL(request.url);
+async function handle(request: ServerRequest): Promise<Response> {
+  const url = new URL(request.url, `http://${request.headers.get("host")}`);
   const id = Math.random().toString(36).substr(2);
 
   if (url.pathname === '/slack') {
@@ -9,7 +11,7 @@ async function handle(request: Request): Promise<Response> {
     const delay = Number(url.searchParams.get('delay'));
 
     if (!hook || !delay) {
-      return new Response("Bad request", { status: 400 });
+      return { body: "Bad Request", status: 400 };
     }
 
     const scheduledAt = new Date().toLocaleString();
@@ -20,12 +22,20 @@ async function handle(request: Request): Promise<Response> {
     }, delay);
     console.log(`${id}: scheduled`);
 
-    return new Response("Scheduled. ID = " + id);
+    return { body: "Scheduled. ID = " + id };
   } else {
-    return new Response("Not found", { status: 404 });
+    return { body: "Not found", status: 404 };
   }
 }
 
-addEventListener("fetch", async (event: any) => {
-  event.respondWith(handle(event.request));
-});
+async function handleAndRespond(request: ServerRequest): Promise<void> {
+  await request.respond(await handle(request));
+}
+
+const port = 8080;
+const server = serve({ hostname: 'localhost', port });
+console.log(`HTTP webserver running.  Access it at:  http://localhost:${port}/`);
+
+for await (const request of server) {
+  handleAndRespond(request);
+}
