5842f554b0006872ba611a6ed141442caea76916
Dan Mercer <danielmercer1@gmail.com>
2021-07-05 14:23:47 -0600

    Add dependency injector

--- main.ts
+++ main.ts
@@ -1,5 +1,7 @@
 
 import { serve, ServerRequest, Response } from "https://deno.land/std@0.100.0/http/server.ts";
+import { AppHandler } from "./app.ts";
+import { makeInjector } from './deps/injector.ts';
 
 if (import.meta.main) {
   const port = 8080;
@@ -7,17 +9,10 @@ if (import.meta.main) {
   console.log(`HTTP webserver running.  Access it at:  http://localhost:${port}/`);
 
   for await (const request of server) {
-    handleAndRespond(handle, request);
+    const inject = makeInjector();
+    const handler = inject(AppHandler);
+    handleAndRespond(handler, request);
   }
-}
-
-async function handle(request: ServerRequest): Promise<Response> {
-  const url = new URL(request.url, `http://${request.headers.get("host")}`);
-  const id = Math.random().toString(36).substr(2);
-
-  // TODO implement /register and /vapidPublicKey
-
-  return { body: "Not found", status: 404 };
 }
 
 async function handleAndRespond(handler: (request: ServerRequest) => Promise<Response>, request: ServerRequest): Promise<void> {
