6ef11bfb675ecab9639803e0288c3980765b1c51
Dan Mercer <danielmercer1@gmail.com>
2021-07-05 17:38:51 -0600

    Revert "Add localStorage test code"
    This reverts commit ea50103cbef0c3458fd6203c54955909aa973d9b.
    
    Deno Deploy doesn't support localStorage, unsurprisingly

--- register.ts
+++ register.ts
@@ -2,7 +2,6 @@ import { injectable } from "./deps/injector.ts";
 
 export const RegisterHandler = injectable(inject => {
   return (url: URL, body: unknown): { body: string, status: number } => {
-    localStorage.setItem("test", url.searchParams.get("test") || 'nothing');
     return {
       body: "yeet " + JSON.stringify(body, null, 2),
       status: 200,
