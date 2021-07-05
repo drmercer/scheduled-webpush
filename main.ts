956b5301a99e494fac255c675acc0a393909177b
Dan Mercer <danielmercer1@gmail.com>
2021-07-05 14:13:00 -0600

    Rearrange main.ts a bit

--- main.ts
+++ main.ts
@@ -1,6 +1,16 @@
 
 import { serve, ServerRequest, Response } from "https://deno.land/std@0.100.0/http/server.ts";
 
+if (import.meta.main) {
+  const port = 8080;
+  const server = serve({ hostname: 'localhost', port });
+  console.log(`HTTP webserver running.  Access it at:  http://localhost:${port}/`);
+
+  for await (const request of server) {
+    handleAndRespond(handle, request);
+  }
+}
+
 async function handle(request: ServerRequest): Promise<Response> {
   const url = new URL(request.url, `http://${request.headers.get("host")}`);
   const id = Math.random().toString(36).substr(2);
@@ -10,14 +20,6 @@ async function handle(request: ServerRequest): Promise<Response> {
   return { body: "Not found", status: 404 };
 }
 
-async function handleAndRespond(request: ServerRequest): Promise<void> {
-  await request.respond(await handle(request));
-}
-
-const port = 8080;
-const server = serve({ hostname: 'localhost', port });
-console.log(`HTTP webserver running.  Access it at:  http://localhost:${port}/`);
-
-for await (const request of server) {
-  handleAndRespond(request);
+async function handleAndRespond(handler: (request: ServerRequest) => Promise<Response>, request: ServerRequest): Promise<void> {
+  await request.respond(await handler(request));
 }
