08b52a31a2cdd25a1f24e53e928cc52d3bac0139
Dan Mercer <danielmercer1@gmail.com>
2021-07-05 18:39:34 -0600

    Naive implementation of scheduled webhooks

--- register.ts
+++ register.ts
@@ -1,26 +1,63 @@
 import { injectable } from "./deps/injector.ts";
-import { sendSlackMessage } from './slack-notify.ts';
+
+async function fire(id: string, hook: string, payload: unknown) {
+  try {
+    const res = await fetch(hook, {
+      method: payload === undefined ? 'GET' : 'POST',
+      body: payload === undefined ? undefined : JSON.stringify(payload),
+    });
+    if (res.ok) {
+      console.log(`${id}: Success! Status code is ${res.status}`);
+    } else {
+      console.warn(`${id}: Failed? Status code is ${res.status}`);
+    }
+  } catch (err) {
+    console.error("Caught error in setTimeout callback!", err);
+  }
+}
 
 export const RegisterHandler = injectable(inject => {
-  return (url: URL, body: unknown): { body: string, status: number } => {
+  return (url: URL, body: any): { body: string, status: number } => {
     const id = Math.random().toString(36).substr(2);
-    const hook = url.searchParams.get('hook');
-    const delay = Number(url.searchParams.get('delay'));
+    let hook: string;
+    try {
+      hook = new URL(body.hook).href;
+    } catch (err) {
+      return {
+        body: "Invalid `hook` URL",
+        status: 400,
+      };
+    }
 
-    if (!hook || !delay) {
-      return { body: "Bad Request", status: 400 };
+    let timestamp = new Date(body.timestamp);
+    if (Number.isNaN(timestamp.getTime())) {
+      return {
+        body: "Invalid `timestamp` time",
+        status: 400,
+      };
+    }
+
+    const payload = body.payload;
+
+    const delay = timestamp.getTime() - Date.now();
+    if (delay > 4*7*24*60*60*1000 || delay < 0) {
+      return {
+        body: "Invalid `timestamp` time",
+        status: 400,
+      };
     }
 
     const scheduledAt = new Date().toLocaleString();
     setTimeout(async () => {
       const sentAt = new Date().toLocaleString();
-      const success = await sendSlackMessage(hook, `${id}: Message scheduled at ${scheduledAt} and sent at ${sentAt}. TEST CHANGE AGAIN`);
-      console.log(`${id}: fired`, { success });
+      console.log(`${id}: Firing. Scheduled at ${scheduledAt} for ${timestamp.toLocaleString()} (current time is ${sentAt}).`);
+      await fire(id, hook, payload);
     }, delay);
-    console.log(`${id}: scheduled`);
 
+    const message = `${id}: Scheduled at ${scheduledAt}.`;
+    console.log(message);
     return {
-      body: `${id}: Scheduled at ${scheduledAt}. TEST CHANGE AGAIN.`,
+      body: message,
       status: 200,
     };
   };
