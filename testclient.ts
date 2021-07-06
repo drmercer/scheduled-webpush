f680b2df7d675da1559fc66afa5045ec8e6093f6
Dan Mercer <danielmercer1@gmail.com>
2021-07-05 18:52:51 -0600

    Add test client script

---
+++ testclient.ts
@@ -0,0 +1,23 @@
+if (import.meta.main) {
+  const deployedEndpoint = new URL(Deno.args[0]).href;
+  const hook = new URL(Deno.args[1], 'https://webhook.site/').href;
+  const delayInSeconds = Number(Deno.args[2]) || 30;
+  const text = Deno.args[3] || "hello there";
+
+  const res = await fetch(deployedEndpoint, {
+    method: 'POST',
+    body: JSON.stringify({
+      hook,
+      timestamp: Date.now() + delayInSeconds * 1000,
+      payload: {
+        text,
+      },
+    }),
+  });
+
+  if (res.ok) {
+    console.log("Success");
+  } else {
+    console.error("Failed", res, await res.text());
+  }
+}
